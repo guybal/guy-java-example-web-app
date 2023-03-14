f81916ef86e39520c876a745c4e824a798f4e7a6
Guy Balmas <guyb@terasky.com>
2023-03-14 10:51:25 +0200

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!\nDeployed on TAP v1.4!";
+		return "Greetings from Spring Boot + Tanzu!\nDeployed on TAP v1.4!!!";
 	}
 
 }
